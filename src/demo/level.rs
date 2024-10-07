70370e44ea14a1abd3df3c4712f9d369454841e5
Feod <miikkaharjuntausta@gmail.com>
2024-10-07 13:06:05 +0300

    removed box as it was not working

--- src/demo/level.rs
+++ src/demo/level.rs
@@ -16,21 +16,4 @@ pub fn spawn_level(world: &mut World) {
     // The only thing we have in our level is a player,
     // but add things like walls etc. here.
     SpawnPlayer { max_speed: 400.0 }.apply(world);
-    spawn_physics_square(world);
-}
-
-fn spawn_physics_square(world: &mut World) {
-    let texture_handle = {
-        let asset_server = world.resource::<AssetServer>();
-        asset_server.load("images/WhiteSquare.png")
-    };
-
-    let mut commands = world.spawn();
-    commands.insert_bundle(SpriteBundle {
-        texture: texture_handle,
-        transform: Transform::from_scale(Vec3::splat(50.0)),
-        ..Default::default()
-    });
-    commands.insert(RigidBody::Dynamic); // P5793
-    commands.insert(Collider::cuboid(25.0, 25.0)); // P6080
 }
